b76f6dad3bd5b527dfab78f38360ff3accfdcac5
Dmitry <bulbamanblr@gmail.com>
2021-07-29 17:27:13 +0200

    Init & add strategy pattern

---
+++ StrategyPattern/Context.cs
@@ -0,0 +1,23 @@
+﻿namespace StrategyPattern
+{
+    public class Context
+    {
+        public ICpu Cpu { get; set; }
+
+        protected CardType Card;
+
+        protected int RamMemory;
+
+        public Context(CardType cardType, int memory, ICpu cpu)
+        {
+            Cpu = cpu;
+            Card = cardType;
+            RamMemory = memory;
+        }
+
+        public void ExecuteProccesing()
+        {
+            Cpu.Process();
+        }
+    }
+}
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var computer = new Context(CardType.GeForce, 16, new AmdRyzen());
+            computer.ExecuteProccesing();
+
+            Task.Delay(1000);
+
+            var computer_2 = new Context(CardType.AmdRadeon, 8, new IntelCore());
+
+            computer_2.ExecuteProccesing();
+
+            Console.Read();
+        }
+    }
+}
